af7384d041162a50df684aeb6beb4acc3111cedf
Nicolas Martyanoff <nicolas@n16f.net>
2023-03-25 20:54:16 +0100

    add support for sending messages

---
+++ pkg/raft/protocol.go
@@ -0,0 +1,142 @@
+package raft
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type RPCMsg interface {
+	GetType() string
+	GetTerm() Term
+
+	fmt.Stringer
+}
+
+type RPCRequestVoteRequest struct {
+	Term         Term
+	CandidateId  ServerId
+	LastLogIndex LogIndex
+	LastLogTerm  Term
+}
+
+func (msg *RPCRequestVoteRequest) GetType() string {
+	return "requestVoteRequest"
+}
+
+func (msg *RPCRequestVoteRequest) GetTerm() Term {
+	return msg.Term
+}
+
+func (msg *RPCRequestVoteRequest) String() string {
+	return fmt.Sprintf("RequestVoteRequest{term: %d, candidateId: %q, "+
+		"lastLogIndex: %d, lastLogTerm: %d}",
+		msg.Term, msg.CandidateId, msg.LastLogIndex, msg.LastLogTerm)
+}
+
+type RPCRequestVoteResponse struct {
+	Term        Term
+	VoteGranted bool
+}
+
+func (msg *RPCRequestVoteResponse) GetType() string {
+	return "requestVoteResponse"
+}
+
+func (msg *RPCRequestVoteResponse) GetTerm() Term {
+	return msg.Term
+}
+
+func (msg *RPCRequestVoteResponse) String() string {
+	return fmt.Sprintf("RequestVoteResponse{term: %d, voteGranted: %v}",
+		msg.Term, msg.VoteGranted)
+}
+
+type RPCAppendEntriesRequest struct {
+	Term         Term
+	LeaderId     ServerId
+	PrevLogIndex LogIndex
+	PrevLogTerm  Term
+	Entries      []LogEntry
+	LeaderCommit LogIndex
+}
+
+func (msg *RPCAppendEntriesRequest) GetType() string {
+	return "appendEntriesRequest"
+}
+
+func (msg *RPCAppendEntriesRequest) GetTerm() Term {
+	return msg.Term
+}
+
+func (msg *RPCAppendEntriesRequest) String() string {
+	return fmt.Sprintf("AppendEntriesRequest{term: %d, leaderId: %q, "+
+		"prevLogIndex: %d, prevLogTerm: %d, %d entries, leaderCommit: %d}",
+		msg.Term, msg.LeaderId, msg.PrevLogIndex, msg.PrevLogTerm,
+		len(msg.Entries), msg.LeaderCommit)
+}
+
+type RPCAppendEntriesResponse struct {
+	Term    Term
+	Success bool
+}
+
+func (msg *RPCAppendEntriesResponse) GetType() string {
+	return "appendEntriesResponse"
+}
+
+func (msg *RPCAppendEntriesResponse) GetTerm() Term {
+	return msg.Term
+}
+
+func (msg *RPCAppendEntriesResponse) String() string {
+	return fmt.Sprintf("AppendEntriesResponse{term: %d, success: %v}",
+		msg.Term, msg.Success)
+}
+
+func EncodeRPCMsg(msg RPCMsg) ([]byte, error) {
+	value := struct {
+		Type  string `json:"type"`
+		Value RPCMsg `json:"value"`
+	}{
+		Type:  msg.GetType(),
+		Value: msg,
+	}
+
+	return json.Marshal(value)
+}
+
+func DecodeRPCMsg(data []byte) (RPCMsg, error) {
+	var value struct {
+		Type  string          `json:"type"`
+		Value json.RawMessage `json:"value"`
+	}
+
+	if err := json.Unmarshal(data, &value); err != nil {
+		return nil, err
+	}
+
+	var msg RPCMsg
+
+	switch value.Type {
+	case "requestVoteRequest":
+		msg = &RPCRequestVoteRequest{}
+
+	case "requestVoteResponse":
+		msg = &RPCRequestVoteResponse{}
+
+	case "appendEntriesRequest":
+		msg = &RPCAppendEntriesRequest{}
+
+	case "appendEntriesResponse":
+		msg = &RPCAppendEntriesResponse{}
+
+	default:
+		return nil, fmt.Errorf("unknown message type %q", value.Type)
+	}
+
+	if err := json.Unmarshal(value.Value, &msg); err != nil {
+		return nil, err
+	}
+
+	return msg, nil
+}
--- pkg/raft/transport.go
+++ pkg/raft/transport.go
@@ -1,10 +1,14 @@
 package raft
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io/ioutil"
 	"net"
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 )
 
@@ -69,4 +73,91 @@ func (s *Server) stopHTTPServer() {
 	defer cancel()
 
 	s.httpServer.Shutdown(ctx)
+}
+
+func (s *Server) sendMsg(recipientId ServerId, msg RPCMsg) error {
+	s.Log.Debug(2, "sending %v to %s", msg, recipientId)
+
+	// Encode the message
+	msgData, err := EncodeRPCMsg(msg)
+	if err != nil {
+		return fmt.Errorf("cannot encode message: %w", err)
+	}
+
+	// Obtain the address of the recipient
+	recipient, found := s.Cfg.Servers[recipientId]
+	if !found {
+		return fmt.Errorf("unknown recipient id %q", recipientId)
+	}
+
+	address := recipient.PublicAddress
+
+	// Create the HTTP request
+	uri := url.URL{
+		Scheme: "http",
+		Host:   string(address),
+	}
+
+	req, err := http.NewRequest("POST", uri.String(), bytes.NewReader(msgData))
+	if err != nil {
+		return fmt.Errorf("cannot create http request: %w", err)
+	}
+
+	req.Header.Set("X-Raft-Source-Id", string(s.Id))
+
+	// Send the request asynchronously to avoid blocking the server
+	go s.sendMsgRequest(address, msg, req)
+
+	return nil
+}
+
+func (s *Server) sendMsgRequest(address ServerAddress, msg RPCMsg, req *http.Request) {
+	defer func() {
+		if value := recover(); value != nil {
+			msg := RecoverValueString(value)
+			trace := StackTrace(10)
+			s.Log.Error("cannot send request: panic: %s\n%s", msg, trace)
+		}
+	}()
+
+	// Send the request and wait for the response
+	res, err := s.httpClient.Do(req)
+	if err != nil {
+		s.Log.Error("cannot send %v to %s: %v", msg, address, err)
+		return
+	}
+	defer res.Body.Close()
+
+	// Check the response status
+	if res.StatusCode != 204 {
+		var msg string
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err == nil {
+			msg = string(body)
+
+			if idx := strings.IndexAny(msg, "\r\n"); idx > 0 {
+				msg = msg[:idx]
+			}
+
+			if msg != "" {
+				msg = ": " + msg
+			}
+		} else {
+			s.Log.Error("cannot read response from %s: %v", address, err)
+		}
+
+		s.Log.Error("http request to %s failed with status %d%s",
+			address, res.StatusCode, msg)
+	}
+}
+
+func (s *Server) broadcastMsg(msg RPCMsg) {
+	for id := range s.Cfg.Servers {
+		if id == s.Id {
+			continue
+		}
+
+		s.sendMsg(id, msg)
+	}
 }
