2b3ad75191aaed63a6f0f7e79656984185226507
Nicolas Martyanoff <nicolas@n16f.net>
2023-03-25 19:43:29 +0100

    add the http client and server

--- pkg/raft/server.go
+++ pkg/raft/server.go
@@ -1,7 +1,12 @@
 package raft
 
 import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
 	"sync"
+	"time"
 )
 
 type ServerCfg struct {
@@ -18,6 +23,9 @@ type Server struct {
 	Id            ServerId
 	LocalAddress  ServerAddress
 	PublicAddress ServerAddress
+
+	httpServer *http.Server
+	httpClient *http.Client
 
 	errChan  chan<- error
 	stopChan chan struct{}
@@ -46,6 +54,13 @@ func (s *Server) Start(errChan chan<- error) error {
 
 	s.errChan = errChan
 
+	if err := s.startHTTPServer(); err != nil {
+		return fmt.Errorf("cannot start http server: %w", err)
+	}
+	s.Log.Info("listening on %s", s.LocalAddress)
+
+	s.httpClient = newHTTPClient()
+
 	s.wg.Add(1)
 	go s.main()
 
@@ -59,6 +74,8 @@ func (s *Server) Stop() {
 
 	close(s.stopChan)
 	s.wg.Wait()
+
+	s.stopHTTPServer()
 
 	s.Log.Info("stopped")
 }
@@ -76,4 +93,67 @@ func (s *Server) main() {
 }
 
 func (s *Server) shutdown() {
+}
+
+func (s *Server) startHTTPServer() error {
+	listener, err := net.Listen("tcp", string(s.LocalAddress))
+	if err != nil {
+		return fmt.Errorf("cannot listen on %s: %w", s.LocalAddress, err)
+	}
+
+	s.Log.Info("listening on %s", s.LocalAddress)
+
+	s.httpServer = &http.Server{
+		Addr:              string(s.LocalAddress),
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+		IdleTimeout:       60 * time.Second,
+	}
+
+	go func() {
+		defer func() {
+			if value := recover(); value != nil {
+				msg := RecoverValueString(value)
+				trace := StackTrace(10)
+				s.Log.Error("panic: %s\n%s", msg, trace)
+			}
+		}()
+
+		if err := s.httpServer.Serve(listener); err != http.ErrServerClosed {
+			s.errChan <- fmt.Errorf("server error: %w", err)
+			return
+		}
+	}()
+
+	return nil
+}
+
+func (s *Server) stopHTTPServer() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	s.httpServer.Shutdown(ctx)
+}
+
+func newHTTPClient() *http.Client {
+	transport := http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+
+		DialContext: (&net.Dialer{
+			Timeout:   10 * time.Second,
+			KeepAlive: 10 * time.Second,
+		}).DialContext,
+
+		MaxIdleConns: 30,
+
+		IdleConnTimeout:       60 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+
+	client := http.Client{
+		Timeout:   10 * time.Second,
+		Transport: &transport,
+	}
+
+	return &client
 }
---
+++ pkg/raft/utils.go
@@ -0,0 +1,48 @@
+package raft
+
+import (
+	"bytes"
+	"fmt"
+	"runtime"
+)
+
+func RecoverValueString(value interface{}) (msg string) {
+	switch v := value.(type) {
+	case error:
+		msg = v.Error()
+	case string:
+		msg = v
+	default:
+		msg = fmt.Sprintf("%#v", v)
+	}
+
+	return
+}
+
+func StackTrace(depth int) string {
+	pc := make([]uintptr, depth)
+
+	// Always skip runtime.Callers and StackTrace
+	nbFrames := runtime.Callers(2, pc)
+	pc = pc[:nbFrames]
+
+	var buf bytes.Buffer
+
+	frames := runtime.CallersFrames(pc)
+	for {
+		frame, more := frames.Next()
+
+		filePath := frame.File
+		line := frame.Line
+		function := frame.Function
+
+		fmt.Fprintf(&buf, "%s\n", function)
+		fmt.Fprintf(&buf, "  %s:%d\n", filePath, line)
+
+		if !more {
+			break
+		}
+	}
+
+	return buf.String()
+}
